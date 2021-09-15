0a0db65f37fce979f70f274a05663704a19d5b76
diniles <dzliashko@gmail.com>
2021-09-15 13:36:31 +0300

    Add: HelloWorld source

--- src/com/dl/junit/HelloWorld.java
+++ src/com/dl/junit/HelloWorld.java
@@ -1,4 +1,11 @@
 package com.dl.junit;
 
 public class HelloWorld {
+    public static String printHello() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(printHello());
+    }
 }
