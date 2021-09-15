dc926e8fb47368569b865be45e6b798ad9fb792e
diniles <dzliashko@gmail.com>
2021-09-15 14:22:49 +0300

    Add: test for HelloWorld finished

--- src/com/dl/junit/helloworld/HelloWorld.java
+++ src/com/dl/junit/helloworld/HelloWorld.java
@@ -1,11 +1,13 @@
 package com.dl.junit.helloworld;
 
 public class HelloWorld {
-    public static String printHello() {
-        return "Hello World!";
+    public static String printHello(String s) {
+        return s;
     }
 
     public static void main(String[] args) {
-        System.out.println(printHello());
+        System.out.println(printHello("Hello World!"));
+        System.out.println(printHello("Hello!"));
+        System.out.println(printHello("World!"));
     }
 }
